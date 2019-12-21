90217a23f6e2be83958df5709cd63e774bd32ddf
Jason <jasonalvarezcoraiza@gmail.com>
2019-12-21 16:28:19 -0500

    Added Basic Scene and Manager
    Basic assets added as well as game manager object for changing the cursor into a paw. Need to replace the current cursors though because there's too much empty space.

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public Texture2D pawCursor;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotspot = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(pawCursor,hotspot, cursorMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
