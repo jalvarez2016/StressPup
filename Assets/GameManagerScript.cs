d3f6d47a7384b6513ac63eabe5ab702f6c6423b6
Jason <jasonalvarezcoraiza@gmail.com>
2020-01-11 13:15:10 -0500

    Placed buttons
    Placed buttons with attached functions over interactable objects. Functions are in manager script with their respective functions only containing logs for later deletion. Need to make cursor smaller and reassign the owners face button to it's own custom function

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,22 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
 
-    public Texture2D pawCursor;
+    public Texture2D activeCursor;
+    public Texture2D inActiveCursor;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotspot = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.SetCursor(pawCursor,hotspot, cursorMode);
+        Cursor.SetCursor(inActiveCursor,hotspot, cursorMode);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Debug.Log(Input.mousePosition);
+    }
+
+    public void painting(){
+        Debug.Log("clicked on painting");
+    }
+
+    public void mug(){
+        Debug.Log("clicking on the mug, written by Jason as was painting");
+    }
+
+    public void face(){
+        Debug.Log("Clicked on the bosses face, you know the painting");
+    }
+
+    public void umbrella(){
+        Debug.Log("umbrella dog");
+    }
+
+    public void changeScene(){
+        var scene = SceneManager.GetActiveScene();
+        Debug.Log("Active Scene is '" + scene.buildIndex + "'.");
+        SceneManager.LoadScene(scene.buildIndex + 1);
     }
 }
