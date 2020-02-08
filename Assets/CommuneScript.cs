1913d0d77ab8f06fb0a804a71d99bb38d4f8df8a
Jason <jasonalvarezcoraiza@gmail.com>
2020-02-08 15:52:59 -0500

    General movement and jumping
    The jumping and transition between office space and balcony is done. Still need to add dialogue for the people in the office.

--- Assets/CommuneScript.cs
+++ Assets/CommuneScript.cs
@@ -1,44 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CommuneScript : MonoBehaviour
 {
-    public GameObject player;
+    
+
+    public Vector3 paintingPos;
     public GameObject glassdoorsInside;
-    public bool hasJumped;
-    public Vector3 leftMovement;
-    public Vector3 rightMovement;
-    public Vector3 jumpForce;
+    public GameObject glassdoorsOutside;
+    public TextMeshProUGUI textBox;
     private Vector3 balconySpawn;
     // Start is called before the first frame update
     void Start()
     {
         //use this position to teleport player to balcony when they interact with the glassdoors
         balconySpawn = new Vector3(-2.04f, -25.47f, -2);
-        hasJumped = true;
+        paintingPos = GameObject.Find("Painting").transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal") == -1){
-            Debug.Log(player.GetComponent<Rigidbody2D>());
-            player.GetComponent<Rigidbody2D>().AddForce(leftMovement);
-        } else if(Input.GetAxisRaw("Horizontal") == 1){
-            player.GetComponent<Rigidbody2D>().AddForce(rightMovement);
-        }
-        if(Input.GetKeyDown(KeyCode.W) && hasJumped){
-            player.GetComponent<Rigidbody2D>().AddForce(jumpForce);
-            hasJumped= false;
+        if(GameObject.Find("Person").transform.position.x > (GameObject.Find("Glassdoors").transform.position.x - 6) && GameObject.Find("Person").transform.position.y > -20){
+            changeText("Press E to go outside");
+            if(Input.GetKeyDown(KeyCode.E)){
+                teleport(balconySpawn);
+            }
+        } else if(GameObject.Find("Person").transform.position.x < (GameObject.Find("Glassdoors (1)").transform.position.x + 10) && GameObject.Find("Person").transform.position.y < -20) {
+            changeText("Press E to go inside");
+            if(Input.GetKeyDown(KeyCode.E)){
+                GameObject.Find("Painting").transform.position = paintingPos;
+                teleport(GameObject.Find("Glassdoors").transform.position);
+            }
+        } else
+        {
+            changeText("Dog: Woof Woof Woof");
         }
     }
 
-       private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.name.Contains("OfficeBackground"))
-        {
-             hasJumped= true;
-        }
+    void changeText(string newTxt)
+    {
+        textBox.GetComponent<TextMeshProUGUI>().text = newTxt;
+    }
+
+    void teleport(Vector3 newPos)
+    {
+        Debug.Log("teleporting");
+        GameObject.Find("Person").transform.position = newPos;
     }
 
 }
---
+++ Assets/playerMovementScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementScript : MonoBehaviour
+{
+    public bool hasJumped;
+    public Vector3 leftMovement;
+    public Vector3 rightMovement;
+    public Vector3 jumpForce;
+    private Vector3 balconySpawn;
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Contains("OfficeBackground") || other.gameObject.name.Contains("Painting"))
+        {
+            Debug.Log("hitting floor");
+                hasJumped= true;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //use this position to teleport player to balcony when they interact with the glassdoors
+        balconySpawn = new Vector3(-2.04f, -25.47f, -2);
+        hasJumped = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") == -1){
+            GetComponent<Rigidbody2D>().AddForce(leftMovement);
+        } else if(Input.GetAxisRaw("Horizontal") == 1){
+            GetComponent<Rigidbody2D>().AddForce(rightMovement);
+        }
+        if(Input.GetKeyDown(KeyCode.W) && hasJumped){
+            GetComponent<Rigidbody2D>().AddForce(jumpForce);
+            hasJumped= false;
+        }
+    }
+}
