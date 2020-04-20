a0c1831efe03b2b7320dbd229f9dfd05743fb471
Jason <jasonalvarezcoraiza@gmail.com>
2020-04-20 18:31:19 -0400

    Woof and animation
    woof and animation have been implemented

---
+++ Assets/videoScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class videoScript : MonoBehaviour
+{
+    public float timer;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 10.0f;
+        canvas = GameObject.Find("Canvas");
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer <=0){
+            canvas.SetActive(true);
+
+        } else {
+            timer -= Time.deltaTime;
+        }
+        
+    }
+}
---
+++ Assets/woofScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woofScript : MonoBehaviour
+{
+    public GameObject woof;
+    public float timer;
+    public int rand; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!SavedVariables.translate){
+            if(timer<=0){            
+                woof.GetComponent<AudioSource>().Play();
+                rand = Random.Range(0,1);
+                if(rand == 1){
+                    woof.GetComponent<AudioSource>().Play();
+                }
+                timer = 5f;
+
+            } else {            
+                timer -= Time.deltaTime;
+            }
+
+        }
+        
+    }
+}
